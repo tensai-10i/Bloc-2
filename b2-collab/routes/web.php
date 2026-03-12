1c36c985f6c0f4a191979dec2567ffe8a9d3af48
ilion <ilianmahzem@orange.fr>
2026-03-12 15:44:06 +0100

    Ajout crud catégorie (controller + model + migration + view + css)

--- b2-collab/routes/web.php
+++ b2-collab/routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RessourcesController;
-
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/ressources', [RessourcesController::class, 'index'])->name('resources.index');
+
+Route::resource('category', CategoryController::class);
