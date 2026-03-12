0544c82a209ad63466b74feda054ffa373df7adf
Maxime Hoarau <hoaraumaxime6@gmail.com>
2026-03-12 15:29:12 +0100

    push preprod

--- b2-collab/routes/web.php
+++ b2-collab/routes/web.php
@@ -5,12 +5,8 @@ use App\Http\Controllers\RessourcesController;
 
 
 Route::get('/', function () {
-<<<<<<< HEAD
     return view('index');
 });
-=======
-    return view('welcome');
-});
+
 
 Route::get('/ressources', [RessourcesController::class, 'index'])->name('resources.index');
->>>>>>> 4a1d282dea329edb0c8042edc7b2a8ed23fa4321
