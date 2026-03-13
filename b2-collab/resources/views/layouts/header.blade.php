ed262144b8acf17868ecd2b352197f7a60a50dba
Maxime Hoarau <hoaraumaxime6@gmail.com>
2026-03-13 16:00:39 +0100

    fix scss

--- b2-collab/resources/views/layouts/header.blade.php
+++ b2-collab/resources/views/layouts/header.blade.php
@@ -1,15 +1,11 @@
 <header>
     <div class="container navbar-wrapper">
         <nav class="navbar">
-
             <a href="{{ url('/') }}">Accueil</a>
-
-            <a href="#">Ressources</a>
-
+            <a href="{{ route('ressources.index') }}">Ressources</a>
             <a href="{{ route('support') }}">Support</a>
-
+            <a href="{{ route('contact') }}">Contact</a>
             <a href="#">Connexion / inscription</a>
-
         </nav>
     </div>
 </header>
